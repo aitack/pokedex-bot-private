709e0ecd6fae0da7b296ec7099b5b6925def1e57
Jiaens <69499657+aitack@users.noreply.github.com>
2024-08-19 19:46:48 +0900

    fix condition

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ async def on_message(message):
         return
 
     query = message.content.strip().lower()
-    if bool(re.match(r"^[a-zA-Z0-9\s'-]+$", query)):
-        pokemon_en_name = query
-    elif query.isdigit():
+    if query.isdigit():
         pokemon_en_name = list(en_jp_df[en_jp_df["number"] == int(query)]["en_name"])[0]
+    elif bool(re.match(r"^[a-zA-Z0-9\s'-]+$", query)):
+        pokemon_en_name = query
     else:
         pokemon_en_name = list(en_jp_df[en_jp_df["jp_name"] == query]["en_name"])[0]
 
